bc95e9719d9688ab16b7670525b633df1ac3794f
Dewerro <funcake0@gmail.com>
2022-12-06 12:05:33 +0300

    Fix issues with card input

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function cardInput(input) {
-    if (input.length % 5 == 0) {
+    if (input.length == 0) // prevents whitespace before input
+        return input
+
+    if (input.length == 20) // prevents whitespace after input
+        return input
+
+    if (input.length % 5 == 0) // set whitespace after each 4 symbols
         return input + " "
-    }
+
     return input
 }
